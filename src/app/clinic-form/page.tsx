58387d167a361b19d291b361eccecc0e061d3731
Andrews Souza <andrewsfs@gmail.com>
2025-05-29 20:25:06 -0300

    feat: add sign in with google

---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,29 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import ClinicForm from "./components/form";
+
+const ClinicFormPage = () => {
+  return (
+    <div>
+      <Dialog open>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Adicionar Unidade</DialogTitle>
+            <DialogDescription>
+              Adicionar uma unidade para continuar.
+            </DialogDescription>
+          </DialogHeader>
+          <ClinicForm />
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
+
+export default ClinicFormPage;
