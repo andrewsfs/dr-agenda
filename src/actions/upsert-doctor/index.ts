d38cb77739490d07c60d202ad79bc77a5704bf3e
Andrews Souza <andrewsfs@gmail.com>
2025-06-01 12:34:02 -0300

    feat: ensure doctor availability is saved as utc on database

--- src/actions/upsert-doctor/index.ts
+++ src/actions/upsert-doctor/index.ts
@@ -1,4 +1,6 @@
 "use server";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
 import { headers } from "next/headers";
 
 import { db } from "@/db";
@@ -8,9 +10,26 @@ import { actionClient } from "@/lib/next-safe-action";
 
 import { upsertDoctorSchema } from "./schema";
 
+dayjs.extend(utc);
+
 export const upsertDoctor = actionClient
   .schema(upsertDoctorSchema)
   .action(async ({ parsedInput }) => {
+    const avaliableFromTime = parsedInput.avaliableFromTime;
+    const avaliableToTime = parsedInput.avaliableToTime;
+
+    const avaliableFromTimeUTC = dayjs()
+      .set("hour", parseInt(avaliableFromTime.split(":")[0]))
+      .set("minute", parseInt(avaliableFromTime.split(":")[1]))
+      .set("second", parseInt(avaliableFromTime.split(":")[2]))
+      .utc();
+
+    const avaliableToTimeUTC = dayjs()
+      .set("hour", parseInt(avaliableToTime.split(":")[0]))
+      .set("minute", parseInt(avaliableToTime.split(":")[1]))
+      .set("second", parseInt(avaliableToTime.split(":")[2]))
+      .utc();
+
     const session = await auth.api.getSession({
       headers: await headers(),
     });
@@ -24,14 +43,18 @@ export const upsertDoctor = actionClient
     await db
       .insert(doctorsTable)
       .values({
+        ...parsedInput,
         id: parsedInput.id,
         clinicId: session?.user.clinic?.id,
-        ...parsedInput,
+        avaliableFromTime: avaliableFromTimeUTC.format("HH:mm:ss"),
+        avaliableToTime: avaliableToTimeUTC.format("HH:mm:ss"),
       })
       .onConflictDoUpdate({
         target: [doctorsTable.id],
         set: {
           ...parsedInput,
+          avaliableFromTime: avaliableFromTimeUTC.format("HH:mm:ss"),
+          avaliableToTime: avaliableToTimeUTC.format("HH:mm:ss"),
         },
       });
   });
